c9fe170bbe7091ce2c034e6017e197d3215c6774
Silvio Tomatis <silvio@tomatis.email>
2023-05-10 10:44:00 +0200

    Initial structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='oltools',
+      version='0.1',
+      description='Tools for Open Library dumps',
+      url='http://github.com/silviot/oltools',
+      author='Flying Circus',
+      author_email='silvio@tomatis.email',
+      license='MIT',
+      packages=['oltools'],
+      zip_safe=False)
