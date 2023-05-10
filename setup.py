644fdb3ee96c31dd34ac890d72e6c2148f494870
Silvio Tomatis <silvio@tomatis.email>
2023-05-10 11:26:43 +0200

    Add ruff linter

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
 
-setup(name='oltools',
-      version='0.1',
-      description='Tools for Open Library dumps',
-      url='http://github.com/silviot/oltools',
-      author='Flying Circus',
-      author_email='silvio@tomatis.email',
-      license='MIT',
-      packages=['oltools'],
-      install_requires=[],
-      extras_require={
-          "test": ["pytest"],
-      },
-      zip_safe=False)
+setup(
+    name="oltools",
+    version="0.1",
+    description="Tools for Open Library dumps",
+    url="http://github.com/silviot/oltools",
+    author="Flying Circus",
+    author_email="silvio@tomatis.email",
+    license="MIT",
+    packages=["oltools"],
+    install_requires=[],
+    extras_require={
+        "test": ["pytest", "ruff"],
+    },
+    zip_safe=False,
+)
